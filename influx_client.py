a2e82d5c1dfe45b2c4022b7e708aee8e39b5047d
Chadi El Masri <chadimsr@gmail.com>
2022-07-10 11:43:30 +0200

    Close client.

--- influx_client.py
+++ influx_client.py
@@ -7,14 +7,14 @@ from influxdb_client import InfluxDBClient, Point
 
 class InfluxClient:
     def __init__(self):
-        client = InfluxDBClient(url=os.environ.get('INFLUX_ENDPOINT'),
+        self.__client = InfluxDBClient(url=os.environ.get('INFLUX_ENDPOINT'),
                                 token=os.environ.get('INFLUX_TOKEN'),
                                 org=os.environ.get('INFLUX_ORG'))
 
         self.__bucket = os.environ.get('INFLUX_BUCKET')
 
-        self.__query_api = client.query_api()
-        self.__write_api = client.write_api(write_options=influxdb_client.client.write_api.SYNCHRONOUS)
+        self.__query_api = self.__client.query_api()
+        self.__write_api = self.__client.write_api(write_options=influxdb_client.client.write_api.SYNCHRONOUS)
 
     def has_cleaned(self, start: datetime, end: datetime):
         start_ts = int(start.timestamp() * 1e9)
@@ -31,3 +31,4 @@ class InfluxClient:
 
     def __exit__(self):
         self.__write_api.close()
+        self.__client.close()

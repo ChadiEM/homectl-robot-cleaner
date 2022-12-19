8422393007425e032068ca70aa4f45f760c5d666
Chadi El Masri <chadimsr@gmail.com>
2022-12-19 20:09:23 +0100

    Interrupt cleaning when hosts arrive.

--- cleaner.py
+++ cleaner.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import time
 
 import requests
 
@@ -14,11 +15,53 @@ ROWENTA_HOSTNAME = os.getenv('ROWENTA_HOSTNAME')
 logger = logging.getLogger(__name__)
 
 
-def clean(influx_client: InfluxClient):
-    response = requests.get(
-        f'http://{ROWENTA_HOSTNAME}:8080/set/clean_map?map_id=3')
+def find_task(command_id):
+    response = requests.get(f'http://{ROWENTA_HOSTNAME}:8080/get/task_history')
 
     if response.ok:
+        history_arr = response.json()['task_history']
+
+        for task in history_arr:
+            if task['source_id'] == command_id:
+                return task
+
+    return None
+
+
+def is_finished(command_id):
+    task = find_task(command_id)
+    if task is None:
+        # shouldn't happen, but just in case
+        return True
+
+    state = task['state']
+
+    if state == 'done' or 'interrupted' in state:
+        return True
+
+    return False
+
+
+def clean(influx_client: InfluxClient, conditions: list[Condition]):
+    response = requests.get(f'http://{ROWENTA_HOSTNAME}:8080/set/clean_map?map_id=3')
+
+    if response.ok:
+        cmd_id = response.json()['cmd_id']
+
+        while True:
+            for condition in conditions:
+                if not condition.is_satisfied():
+                    logger.info(f'Condition {type(condition).__name__} not satisfied. Interrupting.')
+                    requests.get(f'http://{ROWENTA_HOSTNAME}:8080/set/go_home')
+                    return
+
+            if is_finished(cmd_id):
+                break
+
+            time.sleep(10)
+
+        logger.info(f'Cleaning finished. See you tomorrow!')
+
         influx_client.mark_cleaned()
 
 
@@ -30,9 +73,13 @@ def start_if_needed():
             AreHostsOutside()
         ]
 
+        running_conditions: list[Condition] = [
+            AreHostsOutside()
+        ]
+
         for condition in conditions:
             if not condition.is_satisfied():
                 logger.info(f'Condition {type(condition).__name__} not satisfied. Skipping.')
                 return
 
-        clean(influx_client)
+        clean(influx_client, running_conditions)

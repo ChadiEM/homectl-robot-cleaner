5009df9dc46ed0e21977ae4b39d174fe77152f84
Chadi El Masri <chadimsr@gmail.com>
2023-01-30 23:33:47 +0100

    Refactor

--- condition.py
+++ condition.py
@@ -3,5 +3,9 @@ import abc
 
 class Condition(abc.ABC):
     @abc.abstractmethod
-    def is_satisfied(self):
+    def is_satisfied(self) -> bool:
+        pass
+
+    @abc.abstractmethod
+    def should_recheck(self) -> bool:
         pass

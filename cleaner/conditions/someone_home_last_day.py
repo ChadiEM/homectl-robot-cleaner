4fbd585a123e775a1a7a4219f25181d6ed2badaf
Chadi El Masri <chadimsr@gmail.com>
2023-01-31 23:41:10 +0100

    Prepare for tests

--- cleaner/conditions/someone_home_last_day.py
+++ cleaner/conditions/someone_home_last_day.py
@@ -2,9 +2,8 @@ import os
 import time
 from datetime import timedelta
 
-import requests
-
 from cleaner.condition import Condition
+from cleaner.scanner import NetworkScanner
 
 HOME_IPS = os.getenv('HOME_IPS', '')
 NETWORK_SCANNER_ENDPOINT = os.getenv('NETWORK_SCANNER_ENDPOINT')
@@ -13,14 +12,17 @@ NETWORK_SCANNER_ENDPOINT = os.getenv('NETWORK_SCANNER_ENDPOINT')
 class SomeoneHomeInTheLastDay(Condition):
     """If there is no activity in the last 24 hours, do not clean"""
 
+    def __init__(self, scanner: NetworkScanner):
+        self.scanner = scanner
+
     def is_satisfied(self) -> bool:
         current_time = time.time()
         one_day = timedelta(hours=24).total_seconds()
 
         ips = HOME_IPS.split(',')
         for ip in ips:
-            response = requests.get(f'{NETWORK_SCANNER_ENDPOINT}/network/ip/{ip}').json()
-            if response['status'] == 'down' and (current_time - response['last_seen']) < one_day:
+            status_response = self.scanner.get_status(ip)
+            if status_response.status == 'down' and (current_time - status_response.last_seen) < one_day:
                 return True
         return False
 
--- cleaner/main.py
+++ cleaner/main.py
@@ -1,8 +1,16 @@
 import logging
 
 from cleaner import robot_cleaner
+from cleaner.influx_client import InfluxAPIClient
+from cleaner.rowenta_client import RequestsRowentaClient
+from cleaner.scanner import RequestsNetworkScanner
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(name)s %(levelname)s: %(message)s')
 
+logger = logging.getLogger(__name__)
+
 if __name__ == '__main__':
-    robot_cleaner.start_if_needed()
+    logger.info('Started.')
+
+    with InfluxAPIClient() as influx_client:
+        robot_cleaner.start_if_needed(influx_client, RequestsNetworkScanner(), RequestsRowentaClient())

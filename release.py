d86a6c54590f518bc1a4ab8579bdd24cd6136e6e
Chadi El Masri <chadimsr@gmail.com>
2023-10-30 08:25:18 +0100

    Require python 3.11 for tomlib (and allow bumping a type)

--- release.py
+++ release.py
@@ -1,11 +1,17 @@
 import os
 import sys
+import tomllib
 
 
 def release():
-    target_version = sys.argv[1]
+    bump_type = sys.argv[1]
 
-    os.system(f'poetry version {target_version}')
+    os.system(f'poetry version {bump_type}')
+
+    with open('pyproject.toml', 'rb') as f:
+        data = tomllib.load(f)
+        target_version = data['tool']['poetry']['version']
+
     os.system('git add pyproject.toml')
     os.system(f'git commit -m "Bump {target_version}"')
     os.system(f'git tag {target_version}')
